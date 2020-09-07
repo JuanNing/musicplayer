69671c850f79dae9f289869d9743e9be2a0e9f30
xixi <xixi@nodejs.com>
2020-09-07 22:40:56 +0800

    第三章over

--- cloudfunctions/music-m/index.js
+++ cloudfunctions/music-m/index.js
@@ -1,17 +1,32 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
+const TcbRouter = require('tcb-router')
+const rp = require('request-promise')
+const BASE_URL = 'http://musicapi.leanapp.cn'
 cloud.init()
 
 // 云函数入口函数
 exports.main = async(event, context) => {
+    const app = new TcbRouter({ event })
     console.log(event);
-    return await cloud.database().collection('playlist')
-        .skip(event.start)
-        .limit(event.count)
-        .orderBy('createTime', 'desc')
-        .get()
-        .then((res) => {
-            return res
-        })
+    app.router('playlist', async(ctx, next) => {
+        ctx.body = await cloud.database().collection('playlist')
+            .skip(event.start)
+            .limit(event.count)
+            .orderBy('createTime', 'desc')
+            .get()
+            .then((res) => {
+                return res
+            })
+    })
+
+    app.router('musiclist', async(ctx, next) => {
+        console.log(BASE_URL);
+        ctx.body = await rp(BASE_URL + '/playlist/detail?id=' + parseInt(event.playlistId))
+            .then(res => {
+                return JSON.parse(res)
+            })
+    })
+
+    return app.serve()
 }
